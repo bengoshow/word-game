391278bd531c1433cdaa940ca9a65a7a2d7e1ae2
Ben Goshow <ben.goshow@gmail.com>
2023-03-07 18:19:29 -0500

    feat: keep track of guesses with state
    EXERCISE-2

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,15 +2,31 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-import GuessForm from '../GuessForm/GuessForm';
+import GuessForm from '../GuessForm';
+import GuessResults from '../GuessResults';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
+
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <GuessForm />;
+  const [guesses, setGuesses] = React.useState([]);
+  function handleGuesses(guess) {
+    setGuesses([
+      ...guesses,
+      {
+        id: Math.random(),
+        guess: guess
+      }
+    ]);
+  }
+  return (
+    <>
+      <GuessResults guesses={guesses} />
+      <GuessForm handleGuesses={handleGuesses} />
+    </>);
 }
 
 export default Game;
--- src/components/GuessForm/GuessForm.js
+++ src/components/GuessForm/GuessForm.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function GuessForm() {
+function GuessForm({ handleGuesses }) {
   const [guess, setGuess] = React.useState('');
   function handleSubmit(event) {
     event.preventDefault();
     console.info({ guess });
-    setGuess('')
+    setGuess('');
+    handleGuesses(guess);
   }
   return (
     <form className="guess-input-wrapper" onSubmit={handleSubmit}>
@@ -14,7 +15,7 @@ function GuessForm() {
         id="guess-input"
         type="text"
         value={guess}
-        pattern="\w{5}"
+        pattern="[A-Z]{5}"
         minLength="5"
         maxLength="5"
         title="5 letter word"
